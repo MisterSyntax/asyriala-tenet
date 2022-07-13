43328081669dde05443394fd8de7cc67796b008f
MisterSyntax <asyriala@gmail.com>
2022-07-13 00:02:17 -0700

    Move APp and enable feature

---
+++ src/Components/App.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+/** Design System Components */
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+
+/** Components */
+import AppBar from './Layout/AppBar/AppBar';
+import SustainableSavings from './Features/SustainableSavings/SustainableSavings';
+import Footer from './Layout/Footer/Footer';
+
+/** Types */
+
+/** Actions */
+
+/** Selectors */
+
+interface IAppProps {
+}
+
+export function App (props: IAppProps) {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+      }}
+    >
+      <CssBaseline />
+      <AppBar />
+      <Container sx={{ flexGrow: 1, mt: 2, mb: 2 }} maxWidth='md'> 
+        <Box
+          component='main'
+          sx={{ 
+            display: 'flex',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            mt: 8,
+            minHeight: '100%',
+            width: '100%',
+          }}
+        >
+          <SustainableSavings />
+        </Box>
+      </Container>
+      {<Footer />}
+    </Box>
+  );
+}
+
+export default App;
