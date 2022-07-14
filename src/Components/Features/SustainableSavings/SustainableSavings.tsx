1ff1e2613b89b25840127fd8737f0f58ec69687e
MisterSyntax <asyriala@gmail.com>
2022-07-13 23:19:34 -0700

    Add notice if mock apis arent running

--- src/Components/Features/SustainableSavings/SustainableSavings.tsx
+++ src/Components/Features/SustainableSavings/SustainableSavings.tsx
@@ -6,6 +6,7 @@ import Box from '@mui/material/Box';
 import CircularProgress from '@mui/material/CircularProgress';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 
 /** Components */
 import VehicleSelect from './VehicleSelect/VehicleSelect';
@@ -18,7 +19,6 @@ import { fetchTrips } from 'appState/trips/trips.slice';
 import { fetchCosts } from 'appState/costs/costs.slice';
 import SavingsGraph from './SavingsGraph/SavingsGraph';
 import TotalSavings from './TotalSavings/TotalSavings';
-import { Typography } from '@mui/material';
 
 /** Selectors */
 
@@ -28,16 +28,23 @@ interface ISustainableSavingsProps {
 export function SustainableSavings (props: ISustainableSavingsProps) {
   const dispatch = useAppDispatch();
   const [isAppLoaded, setIsAppLoaded] = React.useState(false);
+  const [isDataFetchError, setIsDataFetchError] = React.useState(false);
 
   React.useEffect(() => {
     async function fetchAppData() {
       if(!isAppLoaded) {
-        await Promise.all([
-          dispatch(fetchCars()),
-          dispatch(fetchTrips()),
-          dispatch(fetchCosts())
-        ]);
-        setIsAppLoaded(true);
+        try {
+          const result = await Promise.all([
+            dispatch(fetchCars()).unwrap(),
+            dispatch(fetchTrips()).unwrap(),
+            dispatch(fetchCosts()).unwrap()
+          ]);
+          console.log(result);
+          setIsAppLoaded(true);
+        }
+        catch {
+          setIsDataFetchError(true);
+        }
       }
     }
     fetchAppData();
@@ -48,7 +55,10 @@ export function SustainableSavings (props: ISustainableSavingsProps) {
       <Box>
         <Typography variant='h2' component='h1' sx={{ fontWeight: 'bold' }}>Sustainable Savings</Typography>
         <Box sx={{ height: '50vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-          <CircularProgress />
+          {isDataFetchError ? 
+            <Typography>Unable to fetch data. Please start the api by running "yarn start-api"</Typography>
+            : <CircularProgress />
+          }
         </Box>
       </Box>
     );
@@ -74,7 +84,6 @@ export function SustainableSavings (props: ISustainableSavingsProps) {
           <SavingsGraph />
         </Paper>
       </Grid>
-      {/* Recent Deposits */}
       <Grid item xs={12}>
         <Paper
           sx={{
